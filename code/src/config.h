d8881b9561cff08fdb2a357b36f06b413f38b894
UnHold (Alex) <alex@hold.re>
2025-08-03 18:43:50 +0200

    changed sec to send

--- code/src/config.h
+++ code/src/config.h
@@ -15,7 +15,7 @@ static String RST_USERNAMES[NUM_USER] = {"UnHold", "Luggi", "Tabaluga", "Someone
 #define SLEEP_TIME 10 * 60 //10min // wakeup is defined as current_unix_timestamp mod SLEEP_TIME == 0
 
 #define USER_SEND_OFFSET secToMs(1) //each user send 1 sec after the other
-#define UPDATE_AWAKE_TIME (secToMs(10) + USER_SEND_OFFSET * NUM_USER)
+#define UPDATE_AWAKE_TIME (secToMs(16) + USER_SEND_OFFSET * NUM_USER)
 
 
 #define VERSION "1.0"
